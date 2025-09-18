a537aeb89e0e981442e5cb072d7b617993c541c0
siva100625 <153293464+siva100625@users.noreply.github.com>
2025-09-18 16:45:19 +0530

    Update urls.py

--- sensor_project/sensor_project/urls.py
+++ sensor_project/sensor_project/urls.py
@@ -8,10 +8,8 @@ router.register(r'sensor-data', SensorDataViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api/auth/',include('sensor_data.urls')),
-    path('custom-admin/', admin.site.urls),  # Admin interface at /custom-admin/
-# Include your app's URLs
-  # Endpoint to handle blink request
+    path('custom-admin/', admin.site.urls), 
     path('auth/', include('dj_rest_auth.urls')),
-    path('auth/social/', include('dj_rest_auth.registration.urls')),  # Social login URLs
+    path('auth/social/', include('dj_rest_auth.registration.urls')),
     path('auth/', include('social_django.urls', namespace='social')),
 ]
