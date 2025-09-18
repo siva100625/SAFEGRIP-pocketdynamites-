bc0fad18c4b698fd3ab093521d50ef711051ffc0
siva100625 <153293464+siva100625@users.noreply.github.com>
2025-09-18 16:46:37 +0530

    Update admin.py

--- sensor_project/sensor_data/admin.py
+++ sensor_project/sensor_data/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from . import models
-# Customize the UserAdmin to display only the username
-class UserAdmin(BaseUserAdmin):
-    model = User
-    list_display = ('username',)  # Display only the username field
-    search_fields = ('username',)  # Search by username
+# # Customize the UserAdmin to display only the username
+# class UserAdmin(BaseUserAdmin):
+#     model = User
+#     list_display = ('username',)  # Display only the username field
+#     search_fields = ('username',)  # Search by username
 
-admin.site.unregister(User)  # Unregister the default User admin
-admin.site.register(User, UserAdmin)
+# admin.site.unregister(User)  # Unregister the default User admin
+# admin.site.register(User)
 
 admin.site.register(models.SensorData)
