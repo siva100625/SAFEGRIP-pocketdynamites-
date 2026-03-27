b6f35af20d24e743d5bb4d87ddc9984d4b62005c
siva100625 <153293464+siva100625@users.noreply.github.com>
2026-03-27 18:22:22 +0530

    Update settings.py

--- sensor_project/sensor_project/settings.py
+++ sensor_project/sensor_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&+h(_t^^()-+z4!*u#60lw=(ncxy40eaxn3^so7e*o*cxj^1n5'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
